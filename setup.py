4e18ef0a557b96333ff0ce6e9f34831d837e4fab
Gagaro <gagaro42@gmail.com>
2017-02-13 14:23:05 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='terralego',
+    version='0.1',
+    description='Python bindings for Terralego services.',
+    url='https://terralego.fr',
+    author='Autonomens',
+    author_email='contact@autonomens.fr',
+    license='MIT',
+    packages=['terralego'],
+    zip_safe=False,
+)
