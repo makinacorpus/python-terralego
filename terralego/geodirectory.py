950178b7cc3977419f3c6323e73d6c8f2059d89c
Gagaro <gagaro42@gmail.com>
2017-03-01 16:10:23 +0100

    fix: geodirectory url and geometry

--- terralego/geodirectory.py
+++ terralego/geodirectory.py
@@ -4,24 +4,27 @@ import requests
 from terralego.conf import settings
 
 GEODIRECTORY_URL = settings.TERRALEGO_URL.format(api='geodirectory')
-GEODIRECTORY_ENTRY_URL = '{geodirectory_url}/{{entry_id}}/'.format(geodirectory_url=GEODIRECTORY_URL)
+GEODIRECTORY_ENTRY_LIST_URL = '{geodirectory_url}entries/'.format(geodirectory_url=GEODIRECTORY_URL)
+GEODIRECTORY_ENTRY_DETAIL_URL = '{geodirectory_url}entries/{{entry_id}}/'.format(geodirectory_url=GEODIRECTORY_URL)
 
 
 def create_entry(geometry, tags=None):
     """
     Create a new entry.
 
-    :param geometry: A WKT string representing the geometry of the entry.
+    :param geometry: A WKT string representing the geometry of the entry or a dict representing the geojson.
     :param tags: A list of string describing the entry. Can be used for filtering later on.
     :return: A geojson describing the entry as a python dictionnary.
     """
     if tags is None:
         tags = []
+    if isinstance(geometry, dict):
+        geometry = json.dumps(geometry)
     data = {
         'geometry': geometry,
         'tags': json.dumps(tags),
     }
-    response = requests.post(GEODIRECTORY_URL, data=data, auth=(settings.USER, settings.PASSWORD))
+    response = requests.post(GEODIRECTORY_ENTRY_LIST_URL, data=data, auth=(settings.USER, settings.PASSWORD))
     response.raise_for_status()
     return response.json()
 
@@ -33,7 +36,7 @@ def get_entry(entry_id):
     :param entry_id: The id of the entry.
     :return: A geojson describing the entry as a python dictionnary.
     """
-    url = GEODIRECTORY_ENTRY_URL.format(entry_id=entry_id)
+    url = GEODIRECTORY_ENTRY_DETAIL_URL.format(entry_id=entry_id)
     response = requests.get(url, auth=(settings.USER, settings.PASSWORD))
     response.raise_for_status()
     return response.json()
@@ -44,17 +47,19 @@ def update_entry(entry_id, geometry, tags=None):
     Update an entry.
 
     :param entry_id: The id of the entry.
-    :param geometry: A WKT string representing the geometry of the entry.
+    :param geometry: A WKT string representing the geometry of the entry or a dict representing the geojson.
     :param tags: A list of string describing the entry. Can be used for filtering later on.
     :return: A geojson describing the updated entry as a python dictionnary.
     """
     if tags is None:
         tags = []
+    if isinstance(geometry, dict):
+        geometry = json.dumps(geometry)
     data = {
         'geometry': geometry,
         'tags': json.dumps(tags),
     }
-    url = GEODIRECTORY_ENTRY_URL.format(entry_id=entry_id)
+    url = GEODIRECTORY_ENTRY_DETAIL_URL.format(entry_id=entry_id)
     response = requests.put(url, data=data, auth=(settings.USER, settings.PASSWORD))
     response.raise_for_status()
     return response.json()
@@ -66,7 +71,7 @@ def delete_entry(entry_id):
 
     :param entry_id: The id of the entry.
     """
-    url = GEODIRECTORY_ENTRY_URL.format(entry_id=entry_id)
+    url = GEODIRECTORY_ENTRY_DETAIL_URL.format(entry_id=entry_id)
     response = requests.delete(url, auth=(settings.USER, settings.PASSWORD))
     response.raise_for_status()
 
