d2f878ecffaf9c774fd6ba0ad230a64b54f710a2
Gagaro <gagaro42@gmail.com>
2017-06-08 17:10:21 +0200

    fix: communes

--- terralego/communes.py
+++ terralego/communes.py
@@ -1,34 +1,43 @@
 import requests
+from requests import RequestException
 
 from terralego.conf import settings
 
 COMMUNES_URL = settings.TERRALEGO_URL.format(api='communes')
-
-COMMUNES_INSEE_URL = '{communes_url}insee/{{insee_code}}/'.format(communes_url=COMMUNES_URL)
-COMMUNES_POSTALCODE_URL = '{communes_url}postalcode/{{postal_code}}/'.format(communes_url=COMMUNES_URL)
+COMMUNES_SEARCH_URL = '{communes_url}search/'.format(communes_url=COMMUNES_URL)
 
 
-def get_by_insee_code(insee_code):
+def _request_commune(params):
+    response = requests.get(
+        COMMUNES_SEARCH_URL,
+        params=params,
+        auth=(settings.USER, settings.PASSWORD)
+    )
+    try:
+        response.raise_for_status()
+    except RequestException:
+        return None
+    return response.json()
+
+
+def get_communes(insee_code=None, postal_code=None):
     """
-    Create a new entry.
+    Get a list of towns by insee_code, postal_code or both.
 
     :param insee_code: The INSEE code of the town.
+    :param postal_code: The postal code of the town.
     :return: A dictionnary containing: 'insee_code', 'postal_code', 'name', 'latitude', 'longitude'
     """
-    url = COMMUNES_INSEE_URL.format(insee_code=insee_code)
-    response = requests.get(url, auth=(settings.USER, settings.PASSWORD))
-    response.raise_for_status()
-    return response.json()
-
-
-def get_by_postal_code(postal_code):
-    """
-    Create a new entry.
-
-    :param postal_code: The postal code of the town.
-    :return: A list of dictionnaries containing: 'insee_code', 'postal_code', 'name', 'latitude', 'longitude'
-    """
-    url = COMMUNES_POSTALCODE_URL.format(postal_code=postal_code)
-    response = requests.get(url, auth=(settings.USER, settings.PASSWORD))
-    response.raise_for_status()
-    return response.json()
+    if insee_code is not None and postal_code is not None:
+        towns = _request_commune({'insee_code': insee_code, 'postal_code': postal_code})
+        if towns is not None:
+            return towns
+    if insee_code is not None:
+        towns = _request_commune({'insee_code': insee_code})
+        if towns is not None:
+            return towns
+    if postal_code is not None:
+        towns = _request_commune({'postal_code': postal_code})
+        if towns is not None:
+            return towns
+    return []
